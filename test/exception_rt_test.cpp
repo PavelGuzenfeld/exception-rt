2cd556b72f6b19f8eafbbeb5c7492b3268a2ae3f
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2025-02-01 17:25:28 +0000

    Implement initial CMake configuration and add exception handling library

---
+++ test/exception_rt_test.cpp
@@ -0,0 +1,19 @@
+#include "exceptions-rt/exception.hpp"
+#include <fmt/core.h>
+#include <string>
+
+[[gnu::noinline]] int start() { throw std::string("Hello world"); }
+int main()
+{
+    volatile int return_code = 0;
+    try
+    {
+        return_code = start();
+    }
+    catch (std::string const &exception)
+    {
+        fmt::print("The exception is {}.\n", exception);
+        return_code = -1;
+    }
+    return return_code;
+}
